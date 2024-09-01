0f0842e6ad50d6568dc7de0ae7ee783f3c0ab1a9
Habibur Rahman <habibur88454@gmail.com>
2024-09-01 23:49:44 +0600

    calculate odd and even number different way

---
+++ for.js
@@ -0,0 +1,37 @@
+for (let i = 1; i <= 10; i++) {
+    // console.log(i)
+}
+
+/* 
+ even ba odd number 3 vabe ber kora jai 
+1. direct increment a sorto diye dile
+2. increment a i++ kore tar vitore condition diye check kore
+*/
+
+
+// even system-01
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        // console.log(i)
+    }
+}
+
+// even system-02
+for (let i = 0; i <= 10; i = i + 2) {   // i = i + 2 ==  and  i += 2 same
+    // console.log(i)
+}
+
+
+// odd system-01
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 1) {
+        // console.log(i)
+    }
+}
+
+
+// odd system-02
+for (let i = 1; i <= 10; i += 2) {
+    console.log(i)
+}
+
