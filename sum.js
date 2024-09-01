0d344ab68e5936064994fc4b03a3a549838207b7
Habibur Rahman <habibur88454@gmail.com>
2024-09-01 22:57:48 +0600

    added even and sum

---
+++ sum.js
@@ -0,0 +1,10 @@
+
+let num = 1;
+let total = 0
+while (num <= 10) {
+    num++
+    console.log(num)
+    total = total + num;
+    console.log('total: ',total)
+    
+}
---
+++ while.js
@@ -0,0 +1,6 @@
+
+let num = 1;
+while (num <= 10) {
+    console.log('i am so sorry', num)
+    num++
+}
