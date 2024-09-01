013f8671397178c1fd41b37c362f36fbbc95039b
Habibur Rahman <habibur88454@gmail.com>
2024-09-01 19:47:18 +0600

    for-of loop apply

---
+++ for-of.js
@@ -0,0 +1,14 @@
+
+const numbers = [12, 34, 56, 78, 90];
+console.log(numbers)
+
+for(const number of numbers){
+    console.log(number)
+}
+
+const names = ['azom', 'shahin', 'dipjol', 'kamal'];
+console.log(names);
+
+for(name of names){
+    console.log(name)
+}
