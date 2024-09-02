7d71525e2e8901302feb018cb9a75dfc1e5fa7f8
Habibur Rahman <habibur88454@gmail.com>
2024-09-02 14:09:23 +0600

    problemns solved using for loop

---
+++ task-for.js
@@ -0,0 +1,42 @@
+// task-01;
+// for (let i = 1; i <= 60; i++) {
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!", i);
+// }
+
+
+// task-02
+// Find all the odd numbers from 61 to 100.
+// for (let i = 61; i <= 100; i += 2) {
+//     console.log(i)
+// }
+
+// Find all the even numbers from 78 to 98.
+// for (let i = 78; i <= 98; i += 2) {
+//     console.log(i)
+// }
+
+
+// Display sum of all the odd numbers from 91 to 129.
+// let total = 0;
+// for (let i = 91; i <= 129; i += 2) {
+//     total += i;
+//     console.log(total)
+// }
+// console.log('total', total)
+
+
+// Display sum of all the even numbers from 51 to 85.
+// let total = 0;
+// for (let i = 51; i <= 85; i++) {
+//     if (i % 2 === 0) {
+//         total += i;
+//         console.log(total)
+//     }
+// }
+// console.log('Even total', total)
+
+
+// Implement a countdown timer that counts down from 81 to 65.
+for (let i = 81; i >= 65; i--) {
+    console.log(i)
+}
