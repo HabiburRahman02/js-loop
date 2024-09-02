aacd117c0865cd79a47764bf15a17bf34d11aa33
Habibur Rahman <habibur88454@gmail.com>
2024-09-02 15:21:30 +0600

    break method apply

---
+++ break-task.js
@@ -0,0 +1,20 @@
+// task-01 -->Write a loop 1 to 200. Use break to exit the loop once you find 100.
+// for (let i = 1; i <= 200; i++) {
+//     console.log(i)
+//     if (i >= 100) {
+//         break
+//     }
+// }
+
+
+// Write a while loop that adds numbers starting from 1,
+// but stops (using break) as soon as the sum reaches or exceeds 100
+
+// let n= 1;
+// while(n <= Infinity){
+//     console.log(n)
+//     if(n >= 100){
+//         break;
+//     }
+//     n++
+// }
