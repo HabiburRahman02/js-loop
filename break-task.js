54328bd8a54aec93b8da2f445fad5eb72c1d61c9
Habibur Rahman <habibur88454@gmail.com>
2024-09-02 16:27:40 +0600

    try to solved problems

--- break-task.js
+++ break-task.js
@@ -10,11 +10,25 @@
 // Write a while loop that adds numbers starting from 1,
 // but stops (using break) as soon as the sum reaches or exceeds 100
 
-// let n= 1;
-// while(n <= Infinity){
+// let n = 1;
+// while (n <= Infinity) {
 //     console.log(n)
-//     if(n >= 100){
+//     if (n >= 100) {
 //         break;
 //     }
 //     n++
+// }
+
+
+// Write a loop that goes from 1 to 100, but stops (using break)
+// when it encounters the first square number (like 4, 9, 16, etc.)
+// let squire = 1;
+// for (let i = 1; i <= 10; i++) {
+//     squire = i * i
+ 
+//     if (squire >=4) {
+//         break;
+//     }
+//     console.log(squire)
+   
 // }
